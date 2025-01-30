ac9181956c8317074309ab94792cd747734e82b4
DerickDiasDev <149396302+DerickDiasDev@users.noreply.github.com>
2025-01-30 19:38:50 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Tô cansado de socos. Quando é que vou receber meu abraço?</h1>
+  return <h1>Tô cansado de socos. Quando é que vou receber meu abraço?</h1>;
 }
 
 export default Home;
